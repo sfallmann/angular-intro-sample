ac6b8158b87d20397352d85aa6847ab204dd65bd
sfallmann <sean.fallmann@gmail.com>
2022-01-09 07:55:54 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# angular-hynch5
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-hynch5)
---
+++ src/app/product-list/product-list.component.ts
@@ -0,0 +1,23 @@
+import { Component } from '@angular/core';
+
+import { products } from '../products';
+
+@Component({
+  selector: 'app-product-list',
+  templateUrl: './product-list.component.html',
+  styleUrls: ['./product-list.component.css'],
+})
+export class ProductListComponent {
+  products = products;
+  color = 'red';
+
+  share(msg: String = 'the default value') {
+    window.alert(msg);
+  }
+}
+
+/*
+Copyright Google LLC. All Rights Reserved.
+Use of this source code is governed by an MIT-style license that
+can be found in the LICENSE file at https://angular.io/license
+*/
